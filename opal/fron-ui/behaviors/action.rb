c9368753741f29d10209d54453580baded667a11
Szikszai Gusztáv <guszti5@hotmail.com>
2015-05-11 06:17:13 +0200

    Indentation.

--- opal/fron-ui/behaviors/action.rb
+++ opal/fron-ui/behaviors/action.rb
@@ -5,10 +5,10 @@ module UI
       def self.included(base)
         base.include Behaviors::Keydown
         base.style cursor: :pointer,
-                  '&:focus, &:hover' => {
-                    color: ->  { colors.focus },
-                    outline: :none
-                  }
+                   '&:focus, &:hover' => {
+                     color: ->  { colors.focus },
+                     outline: :none
+                   }
 
         base.keydown [:enter, :space], :action
       end
