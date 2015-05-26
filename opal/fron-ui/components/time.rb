6e66c7af17297360722272996410239158e3612e
Szikszai Gusztáv <guszti5@hotmail.com>
2015-05-26 18:24:40 +0200

    Time docs and specs.

--- opal/fron-ui/components/time.rb
+++ opal/fron-ui/components/time.rb
@@ -1,14 +1,26 @@
 module UI
+  # Time component to display static times or
+  # times that counts upwards (fromNow), using [Moment.js](http://momentjs.com/).
+  #
+  # @attr from_now [Bolean] The use the fromNow or not
+  # @attr format [String] The format to display
+  #
+  # @author Gusztáv Szikszai
+  # @since 0.1.0
   class Time < Fron::Component
     tag 'ui-time'
 
     attr_accessor :from_now, :format
 
+    # Initializes the component
     def initialize
       super
       render
     end
 
+    # Sets the value (time) of the component
+    #
+    # @param value [Date, String] The value
     def value=(value)
       @value = if value.is_a? Date
                  Native(`moment(#{value}.date)`)
@@ -21,19 +33,30 @@ module UI
       render
     end
 
+    # Returns the value
+    #
+    # @return [Native] The moment wrapped value
     def value
       (@value || Native(`moment()`))
     end
 
+    # Renders the component
     def render
       clear_timeout @id
       request_animation_frame do
         self.text = if @from_now
                       value.fromNow
                     else
-                      self.text = value.format(@format || 'YYYY-MM-DD')
+                      value.format(@format || 'YYYY-MM-DD')
                     end
       end
+      recall
+    end
+
+    private
+
+    # Periodically calls render
+    def recall
       @id = timeout 1000 do
         render
       end
---
+++ spec/components/time_spec.rb
@@ -0,0 +1,35 @@
+require 'spec_helper'
+
+# Stub timeout so we don't go into recursion
+class UI::Time < Fron::Component
+  def timeout
+  end
+end
+
+describe UI::Time do
+  describe '#recall' do
+    it 'should call render' do
+      subject.should receive(:render)
+      subject.should receive(:timeout).and_yield
+      subject.recall
+    end
+  end
+
+  describe '#value=' do
+    it 'should set value' do
+      subject.value = Date.parse('2015-01-01')
+      subject.text.should eq '2015-01-01'
+    end
+  end
+
+  context 'From now' do
+    before do
+      subject.from_now = true
+      subject.render
+    end
+
+    it 'should render time' do
+      subject.text.should eq 'a few seconds ago'
+    end
+  end
+end
